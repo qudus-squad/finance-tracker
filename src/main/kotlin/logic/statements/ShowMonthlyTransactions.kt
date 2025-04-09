714b495a3a366b3ab13ad985c69e43d5e988aa45
Ibrahim Salem <ibrahim.deep.dev@gmail.com>
2025-04-09 21:34:36 +0200

    implement ShowMonthlyTransactions class to filter and display monthly transactions in CLI

--- src/main/kotlin/logic/statements/ShowMonthlyTransactions.kt
+++ src/main/kotlin/logic/statements/ShowMonthlyTransactions.kt
@@ -1,6 +1,42 @@
 package org.qudus.squad.logic.statements
 
-class ShowMonthlyTransactions {
+import java.text.SimpleDateFormat
+
+class ShowMonthlyTransactions(private val transactionObject: Transaction) {
+
+    private fun getTransactionsByMonth(month: String, year: Int): List<Transaction> {
+        val monthEnum = Month.fromString(month) ?: return emptyList()
+
+        return transactionObject.getAllTransactions()
+            .filter { element ->
+                (element.date.month == monthEnum.value) && (element.date.year + 1900 == year)
+            }
+            .sortedBy { element ->
+                if (element.isIncome) 0 else 1
+            }
+    }
+
+    fun displayMonthlySheet(month: String, year: Int) {
+        val transactions = getTransactionsByMonth(month, year)
+
+        if (transactions.isEmpty()) {
+            println("No transactions found for $month $year.")
+            return
+        }
+
+        println("Your ${month.uppercase()} Monthly Sheet Is:")
+
+        val dateFormat = SimpleDateFormat("dd-MMM-yyyy")
+
+        for (transaction in transactions) {
+            val typeOfTransaction = if (transaction.isIncome) "Income" else "Expense"
+            val formattedDateForTransaction = dateFormat.format(transaction.date)
+            println("$typeOfTransaction: ${transaction.amount} USD   -  ${transaction.category}  -  $formattedDateForTransaction")
+        }
+
+        println("Choose 0 to continue")
+
+    }
 }
 
 enum class Month(val value: Int) {
