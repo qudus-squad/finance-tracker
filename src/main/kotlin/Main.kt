6002531f4f5f6c97006c08e3f28247b98bdcac82
fara7 <fara734isgreat@gmail.com>
2025-04-10 15:30:29 +0300

    Test cases for delete transaction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 package org.qudus.squad
 
+import org.qudus.squad.dataSource.FinanceTrackerDataSourceImpl
+import org.qudus.squad.logic.models.Transaction
+import org.qudus.squad.logic.transactions.DeleteTransaction
+
 fun main() {
 
-    /*TODO*/
+
+
 }
--- src/main/kotlin/testCases/TestCases.kt
+++ src/main/kotlin/testCases/TestCases.kt
@@ -1,3 +1,40 @@
 package org.qudus.squad.testCases
 
-/*TODO ADD YOUR TEST CASES FILES IN THIS PACKAGE*/
+import org.qudus.squad.dataSource.FinanceTrackerDataSourceImpl
+import org.qudus.squad.logic.models.Category
+import org.qudus.squad.logic.models.Transaction
+import org.qudus.squad.logic.transactions.DeleteTransaction
+
+fun main(){
+    val dataSource= FinanceTrackerDataSourceImpl()
+    val transaction= Transaction(1,"Expense",100.0,2005L, Category("Food",1))
+    dataSource.addTransaction(transaction)
+     check(
+        "Valid delete :Transaction is deleted",
+         dataSource.removeTransaction(1),
+         true
+     )
+    check(
+        "Invalid delete :Transaction dose not exist",
+        dataSource.removeTransaction(1),
+        false
+    )
+    val emptyDataSource=FinanceTrackerDataSourceImpl()
+    check(
+        "Invalid delete :No transactions to delete",
+        emptyDataSource.removeTransaction(1),
+        false
+    )
+
+
+}
+
+
+
+fun check(value:String,result:Boolean,correctResult:Boolean){
+    if (result==correctResult){
+        println("Success -$value")
+    }else{
+        println("Failed -$value")
+    }
+}
