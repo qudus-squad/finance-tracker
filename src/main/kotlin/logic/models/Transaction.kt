9f1e2548a25d9556a76b14c13a92e154d476d1b7
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-10 18:23:54 +0200

    test cases for Edit Transaction Date

--- src/main/kotlin/logic/models/Transaction.kt
+++ src/main/kotlin/logic/models/Transaction.kt
@@ -2,8 +2,10 @@ package org.qudus.squad.logic.models
 
 data class Transaction(
     val id: Int,
-    val type: String,
-    val amount: Double,
-    val timeStamp: Long,
-    val category: Category
+    var type: String,
+    var amount: Double,
+    var timeStamp: Long,
+    var category: Category
 )
+{
+}
