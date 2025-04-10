5a2ae42f67d22f3846902bae1dad20e1c936a4de
fara7 <fara734isgreat@gmail.com>
2025-04-10 13:29:02 +0300

    Delete Existing Transaction

--- src/main/kotlin/logic/transactions/DeleteTransaction.kt
+++ src/main/kotlin/logic/transactions/DeleteTransaction.kt
@@ -1,4 +1,18 @@
 package org.qudus.squad.logic.transactions
 
-class DeleteTransaction {
+import org.qudus.squad.dataSource.FinanceTrackerDataSourceImpl
+import org.qudus.squad.logic.FinanceTrackerDataSource
+
+class DeleteTransaction(private val dataSource: FinanceTrackerDataSourceImpl) {
+    fun deleteTransaction(transactionId: Int):Boolean{
+        val transaction=dataSource.getTransactionById(transactionId)
+        return if(transaction !=null){
+            dataSource.removeTransaction(transactionId)
+            true
+        }else{
+            false
+        }
+    }
+
+
 }
