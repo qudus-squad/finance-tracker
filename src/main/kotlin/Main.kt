894085106b8c96df4c43a412b47ba8cc2e89fbab
Mahmoud A. El Yamany <mahmoud.elyamany.official@gmail.com>
2025-04-11 09:45:27 +0200

    implement delete category from datasource

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 package org.qudus.squad
 
+import org.qudus.squad.testCases.testDeleteCategoryFunction
 import org.qudus.squad.testCases.testMonthlyTransactionsCases
 
 
 fun main() {
-    testMonthlyTransactionsCases()
-
+//    testMonthlyTransactionsCases()
+    testDeleteCategoryFunction()
     /*TODO*/
 }
--- src/main/kotlin/logic/categories/DeleteCategory.kt
+++ src/main/kotlin/logic/categories/DeleteCategory.kt
@@ -1,4 +1,11 @@
 package org.qudus.squad.logic.categories
 
-class DeleteCategory {
+import org.qudus.squad.logic.FinanceTrackerDataSource
+import org.qudus.squad.logic.models.Category
+
+class DeleteCategory(private val dataSource: FinanceTrackerDataSource) {
+
+    fun removeCategory(category: Category) : Boolean {
+        return dataSource.removeCategory(category.id)
+    }
 }
