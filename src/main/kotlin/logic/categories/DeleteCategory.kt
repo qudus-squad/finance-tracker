cd4f53c87dc99fea4c86775c5457fcd34368af81
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-12 00:07:23 +0200

    delete the main fun and the rename function name

--- src/main/kotlin/logic/categories/DeleteCategory.kt
+++ src/main/kotlin/logic/categories/DeleteCategory.kt
@@ -2,15 +2,10 @@ package org.qudus.squad.logic.categories
 
 import org.qudus.squad.logic.FinanceTrackerDataSource
 import org.qudus.squad.logic.models.Category
-import org.qudus.squad.testCases.testDeleteCategoryFunction
 
 class DeleteCategory(private val dataSource: FinanceTrackerDataSource) {
 
-    fun removeCategory(category: Category) : Boolean {
+    fun deleteCategory(category: Category): Boolean {
         return dataSource.removeCategory(category.id)
     }
-}
-
-fun main() {
-    testDeleteCategoryFunction()
 }
