3b914f3b86a28b6ba615007f291487da4ed39b9f
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-12 09:46:52 +0200

    rename function name to deleteCategory and remove function

--- src/main/kotlin/logic/categories/DeleteCategory.kt
+++ src/main/kotlin/logic/categories/DeleteCategory.kt
@@ -4,7 +4,6 @@ import org.qudus.squad.logic.FinanceTrackerDataSource
 import org.qudus.squad.logic.models.Category
 
 class DeleteCategory(private val dataSource: FinanceTrackerDataSource) {
-
     fun deleteCategory(category: Category): Boolean {
         return dataSource.removeCategory(category.id)
     }
