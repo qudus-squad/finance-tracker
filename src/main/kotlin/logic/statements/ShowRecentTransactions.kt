d8d21f8f1a380d6bf27c3b625fba219bea223319
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-04-12 10:44:56 +0200

    rename function name and function to show last five recent transactions

--- src/main/kotlin/logic/statements/ShowRecentTransactions.kt
+++ src/main/kotlin/logic/statements/ShowRecentTransactions.kt
@@ -3,9 +3,9 @@ package org.qudus.squad.logic.statements
 import org.qudus.squad.logic.FinanceTrackerDataSource
 import org.qudus.squad.logic.models.Transaction
 
-abstract class ShowRecentTransactions (private val dataSource: FinanceTrackerDataSource) {
+class ShowRecentTransactions (private val dataSource: FinanceTrackerDataSource) {
 
-    fun showTransactions():List<Transaction> {
+    fun showLastFiveRecentTransactions():List<Transaction> {
        return dataSource.getAllTransactions()
     }
 
